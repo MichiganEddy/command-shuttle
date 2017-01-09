c21c7eb2e4c7e5d8a708cdc228a7b26fec035360
Chris Carlson <Chris@cta.cloud>
2017-01-09 13:27:02 -0500

    Added a readme.

---
+++ README.md
@@ -0,0 +1,6 @@
+#command_shuttle
+Shuttle bash commands using node.
+
+A toy project. Very buggy and unstable.
+
+To try it, place the commands bash commands to run in a file, (commands.list), then us '''node run.js''' to run the program. 
--- run.js
+++ run.js
@@ -1,56 +1,99 @@
-var rl = require('readline');
+var readline = require('readline');
 var fs = require('fs');
-var spawn = require('child_process').spawn;
+var execSync = require('child_process').execSync;
 
-rl.setPrompt('~>');
-
-
-var greeter = rl.createInterface({
+rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  prompt: '~>'
 });
-var cmdFile;
-var get_file = function(){
+
+var Intro = function(next){
+  rl.question("Want to run some functions?\n", function(ret){
+  rl.prompt();
+  if(ret.trim() !== 'n'){
+    next();
+  }else {
+      rl.close();
+      process.exit(0);
+  }
+  });
+}
+
+
+
+var run_c = function(cmd){
+  execSync(cmd, { stdio: 'inherit', shell: '/bin/bash', encoding: 'utf8' });
+}
+
+var c_handler = function(commands)
+{
+    while(commands.length >= 0)
+    {
+      var cmd = commands.shift();
+      console.log("Running " + cmd + ".\n");
+      run_c(cmd);
+    }
+      console.log("Thanks, let me know if I can help again later.");
+      rl.close();
+      process.exit(0);
 
 }
 
-greeter.on('badFile', function(){
-  greeter.resume();
-  console.log('did you want to enter the fileName in again?  \(enter \'y\' to try it again. Any other key to quit.\)');
-
-});
-
-
-greeter.on('line', function(resp){
-    if(resp !== 'y')
+var f_confirm = function(contents)
+{
+  console.log("The file you selected contains: \n");
+  console.log(contents);
+  rl.question("Should I run these commands for you?", function(resp){
+    if(resp.trim() !== 'n')
     {
-      console.log("Bye");
+      console.log("Okay Running Commands now. Please wait. \n");
+      c_handler(contents.split('\n'));
+    }
+    else{
+      Intro(FileN());
+    }
+  });
+}
+
+var getF = function(filename){
+  if(fs.existsSync(filename))
+  {
+    console.log("The file " + filename + " exists.");
+    var f_stream = fs.readFile(filename, function(err, data){
+      if(err)
+      {
+        console.log("Error Opening File " + filename + ".\n");
+        console.console.error(err.toString());
+        rl.close();
+        process.exit(1);
+      }
+      f_confirm(data.toString());
+    });
+  }
+  else{
+    console.log("Whoops, I couldn't find " + filename + ". \n");
+    FileN();
+  }
+}
+
+
+var FileN = function(next) {
+    rl.question("What's the name of the file you want to use?", function(ret){
+    rl.prompt();
+    if(ret)
+    {
+    console.log("Okay, Fetching " + ret + " now");
+    getF(ret);
+    }
+    else{
+      console.log("Okay, next time");
+      rl.close();
       process.exit(0);
     }
-    greeter.pause();
-});
 
-var librarian = rl.createInterface({
-
-})
-
-librarian.on('goodfile', function(fileName)
-{
+  });
+}
 
 
-});
-
-var welcome = rl.question("Welcome, want to run some commands?", function(auth){
-  console.log(auth);
-});
-
-var fileGetter = rl.question("What file should I use?", function(lrl){
-    if(fs.existsSync(lrl))
-    {
-      confCommands(lrl);
-    }
-    Console.log("The file " + lrl + "does not exist");
-});
-
-
-wecome();
+Intro(FileN);
